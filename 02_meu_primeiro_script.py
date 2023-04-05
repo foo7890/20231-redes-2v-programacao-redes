7b4596a2ae0a7b68fdfe3d6d6be720c622fbeff6
foo7890 <vinicius.vieira@escolar.ifrn.edu.br>
2023-04-05 15:23:10 -0300

    Adicionando ao script, interação com variáveis de tipos diferentes, resultando em comportamentos de saída diferentes

--- 02_meu_primeiro_script.py
+++ 02_meu_primeiro_script.py
@@ -1,3 +1,11 @@
-import sys
+#Importando um módulo 
 
-print(sys.platform)
+import sys #utilizar este módulo, para conseguir ler a versão do sistema operacional
+
+print(sys.platform) #impressão do nome S.O.
+
+#brincando com impressão e variáveis
+print(2**100) #impressão da potência de 2 elevado a 100
+
+x="Spam!"
+print(x*8)
